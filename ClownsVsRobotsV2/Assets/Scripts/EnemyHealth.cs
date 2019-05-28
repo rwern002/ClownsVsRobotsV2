834e07aeb04c926eb0b9d9be96e4d62c914b795e
wspalding <thisisspalding@gmail.com>
2019-05-28 10:48:30 -0700

    added enemy2 again

--- ClownsVsRobotsV2/Assets/Scripts/EnemyHealth.cs
+++ ClownsVsRobotsV2/Assets/Scripts/EnemyHealth.cs
@@ -6,6 +6,8 @@ public class EnemyHealth : MonoBehaviour
 {
     public int health = 1000;
     public int score = 10;
+    public int num_enemies_spawned_on_death;
+    public GameObject enemy_prefab;
     PlayerHealth playerHealth;                  // Reference to the player's health.
     GameObject player;                          // Reference to the player GameObject.
 
@@ -24,6 +26,16 @@ public class EnemyHealth : MonoBehaviour
         if (health <= 0)
         {
             playerHealth.AddScore(score);
+            if(enemy_prefab)
+            {
+                for(int i = 0; i < num_enemies_spawned_on_death ; i++)
+                {
+                    float rand_x = Random.Range(-5.0f, 5.0f);
+                    float rand_z = Random.Range(-5.0f, 5.0f);
+                    Vector3 pos = new Vector3(transform.position.x + rand_x, transform.position.y, transform.position.z + rand_z);
+                    Instantiate(enemy_prefab, pos, Quaternion.identity);
+                }
+            }
             Destroy(gameObject);
         }
     }
---
+++ ClownsVsRobotsV2/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int wave_num;
+    public bool between_waves;
+    public GameObject level;
+    public GameObject player;
+    public Transform UI;
+
+    void Start()
+    {
+        wave_num = 0;
+        between_waves = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	//start wave
+        if(!between_waves && Input.GetKeyDown("enter"))
+        {
+        	start_wave();
+        }
+    }
+
+    void start_spawner()
+    {
+
+    }
+
+    void set_spawn_parameters()
+    {
+    	float spawn_treshhold = 1 / 1 + Mathf.Exp(-1 * (wave_num - 5));
+
+    }
+
+    void start_wave()
+    {
+    	between_waves = false;
+    }
+
+    bool wave_over()
+    {
+    	wave_num++;
+    	return false;
+    }
+}
