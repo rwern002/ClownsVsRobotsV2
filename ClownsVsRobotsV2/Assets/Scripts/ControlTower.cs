ef73ee4db390b1a175d08484ecef7f06e9723950
rwern002 <rwern002@ucr.edu>
2019-06-01 16:50:07 -0700

    Added Rotem's Stuff to Scene
    Added new turret, projectile capablities, and controlling towers

---
+++ ClownsVsRobotsV2/Assets/Scripts/ControlTower.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject robot;
+    private Camera mCam;
+    public GameObject tCamObject;
+    public Camera tCam;
+    private bool firstCam;
+    private float speed = 5.0f;
+    public GameObject projectile;
+
+    void Start()
+    {
+        firstCam = true;
+        mCam = GetComponent<Camera>();
+        Debug.Log("mCam: " + mCam.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            mCam.enabled = false;
+            mCam.gameObject.SetActive(false);
+            robot.GetComponent<PlayerMovement>().enabled = true;
+            tCamObject.SetActive(true);
+            tCam.enabled = true;
+            firstCam = false;
+        }
+
+        if (firstCam == true)
+        {
+            mCam.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(projectile, new Vector3(this.transform.position.x + 2f, this.transform.position.y - 1f, this.transform.position.z), Quaternion.identity) as GameObject;
+            Debug.Log("Bullet: " + bullet.transform.position);
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 75);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * 5f, Input.GetAxis("Mouse X") * 5f, 0) * Time.deltaTime * speed);
+    }
+}
--- ClownsVsRobotsV2/Assets/Scripts/PlayerMovement.cs
+++ ClownsVsRobotsV2/Assets/Scripts/PlayerMovement.cs
@@ -5,81 +5,103 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Transform cam;
-
     public float speed;
     private Rigidbody rb;
     public GameObject towerPrefab;
+    private bool foundTower;
     public Animator robotMove;
     private BuildScript getSpawn;
-    //public Camera mCam;
-    //public GameObject tCamObject;
-    //public Camera tCam;
+    public GameObject robot;
+    public GameObject tCamObject;
+    public Camera tCam;
+    private Camera cam;
+
     void Start()
     {
         speed = 20f;
         rb = GetComponent<Rigidbody>();
         getSpawn = FindObjectOfType<BuildScript>();
+        foundTower = false;
     }
 
     void Update()
     {
-        
         if (Input.GetKeyDown(KeyCode.E) && getSpawn.spawnforPlayer == true)
         {
             Instantiate(towerPrefab);
         }
 
-        /*if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && foundTower == true)
         {
-
-            mCam.enabled = !mCam.enabled;
+            Debug.Log("Pressed R");
+            cam.enabled = true;
+            cam.gameObject.SetActive(true);
+            robot.GetComponent<PlayerMovement>().enabled = false;
             tCamObject.SetActive(false);
-            tCam.enabled = !tCam.enabled;
-        }*/
+            tCam.enabled = false;
+        }
+
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horMove = Input.GetAxisRaw("Horizontal");
-        float vertMove = Input.GetAxisRaw("Vertical");
+        float horMove = Input.GetAxis("Horizontal");
+        float vertMove = Input.GetAxis("Vertical");
 
         if (vertMove < 0.0)
         {
-            Debug.Log("Going Back");
+            //Debug.Log("Going Back");
         }
 
         else if (vertMove > 0.0)
         {
 
         }
-
-        //Changed by Jasiel ------------------------------------------------------------
-        Vector3 camF = cam.forward;
-        Vector3 camR = cam.right;
+        Vector3 camF = tCam.transform.forward;
+        Vector3 camR = tCam.transform.right;
         camF.y = 0;
         camR.y = 0;
         camF = camF.normalized;
         camR = camR.normalized;
-        Vector3 groundForward = new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z).normalized;
-        Vector3 groundRight = new Vector3(cam.transform.right.x, 0, cam.transform.right.z).normalized;
+        Vector3 groundForward = new Vector3(tCam.transform.transform.forward.x, 0, tCam.transform.transform.forward.z).normalized;
+        Vector3 groundRight = new Vector3(tCam.transform.transform.right.x, 0, tCam.transform.transform.right.z).normalized;
         Vector3 movement = groundForward * Input.GetAxisRaw("Vertical") + groundRight * Input.GetAxisRaw("Horizontal");
         movement = movement.normalized;
-        //Vector3 movement = new Vector3(horMove, 0.0f, vertMove);
-        //movement = Camera.main.transform.TransformDirection(movement);
-        //movement.y = 0.0f;
-        //transform.Translate(movement * Time.deltaTime * speed);
+
+        /*Vector3 movement = new Vector3(horMove, 0.0f, vertMove);
+        movement = Camera.main.transform.TransformDirection(movement);
+        movement.y = 0.0f;
+        transform.Translate(movement * Time.deltaTime * speed);*/
         if (movement != Vector3.zero)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
             robotMove.SetFloat("Speed", 1.0f);
         }
+
         else
         {
             robotMove.SetFloat("Speed", 0.0f);
         }
         transform.Translate(movement * Time.deltaTime * speed, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ControlT")
+        {
+            Debug.Log("Tower Found: " + other.gameObject.name);
+            //Debug.Log("Camera Found: " + cam.name);
+            cam = other.transform.GetChild(2).gameObject.GetComponent<Camera>();
+            foundTower = true;
+            //Debug.Log("Camera: " + cam.gameObject.name);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        foundTower = false;
     }
 
 }
